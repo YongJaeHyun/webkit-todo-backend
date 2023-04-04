8e93e2fb346298e246868d90b40948df8a17d9a6
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-04-04 20:04:50 +0900

    26. 패스워드 암호화

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.todo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +24,15 @@ public class UserController {
 	@Autowired
 	private TokenProvider tokenProvider;
 	
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
 		try {
 			UserEntity user = UserEntity.builder()
 										.email(userDTO.getEmail())
 										.username(userDTO.getUsername())
-										.password(userDTO.getPassword())
+										.password(passwordEncoder.encode(userDTO.getPassword()))
 										.build();
 			
 			UserEntity registeredUser = userService.create(user);
@@ -47,7 +51,11 @@ public class UserController {
 	
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
-		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+		UserEntity user = userService.getByCredentials(
+										userDTO.getEmail(),
+										userDTO.getPassword(),
+										passwordEncoder);
+		
 		if(user != null) {
 			final String token = tokenProvider.create(user);
 			final UserDTO responseUserDTO = UserDTO.builder()
--- src/main/java/com/example/todo/service/UserService.java
+++ src/main/java/com/example/todo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.todo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.todo.model.UserEntity;
@@ -23,10 +24,15 @@ public class UserService {
 			log.warn("Email already exists {}", email);
 			throw new RuntimeException();
 		}
+		
 		return userRepository.save(userEntity);
 	}
 	
-	public UserEntity getByCredentials(final String email, final String password) {
-		return userRepository.findByEmailAndPassword(email, password);
+	public UserEntity getByCredentials(final String email, final String password, final PasswordEncoder encoder) {
+		final UserEntity originalUser = userRepository.findByEmail(email);
+		if(originalUser != null && encoder.matches(password, originalUser.getPassword())) {
+			return originalUser;
+		}
+		return null;
 	}
 }
